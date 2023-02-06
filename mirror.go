e6294d41bd9dc2098160118bd1c4d0e943a0fe9d
Kamaleshwar <bn_kamalesh@yahoo.com>
2023-02-06 15:36:02 +0100

    [minor] mirrored Go v1.20 errors.Join method [-] removed travis and using github workflow

--- mirror.go
+++ mirror.go
@@ -16,3 +16,47 @@ func Is(err, target error) bool {
 func As(err error, target interface{}) bool {
 	return errors.As(err, target)
 }
+
+// Join returns an error that wraps the given errors.
+// This is the exact implementation of Go v1.20.
+// It will be removed when Go >= v1.20 becomes the LTS version, and would just call the
+// native Join after that.
+func Join(errs ...error) error {
+	n := 0
+	for _, err := range errs {
+		if err != nil {
+			n++
+		}
+	}
+	if n == 0 {
+		return nil
+	}
+	e := &joinError{
+		errs: make([]error, 0, n),
+	}
+	for _, err := range errs {
+		if err != nil {
+			e.errs = append(e.errs, err)
+		}
+	}
+	return e
+}
+
+type joinError struct {
+	errs []error
+}
+
+func (e *joinError) Error() string {
+	var b []byte
+	for i, err := range e.errs {
+		if i > 0 {
+			b = append(b, '\n')
+		}
+		b = append(b, err.Error()...)
+	}
+	return string(b)
+}
+
+func (e *joinError) Unwrap() []error {
+	return e.errs
+}
